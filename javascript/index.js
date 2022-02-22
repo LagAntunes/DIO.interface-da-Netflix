81efee869ffc0dc89647d9625609761636657209
LagAntunes <lag151617@gmail.com>
2022-02-21 21:06:12 -0300

    adicionando titulos e funcionalidades ao carrossel

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,18 @@
-/* início da lógica usada no header */
-   
-/* fim da lógica usada no header */
+/* início da lógica usada no carrossel */
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+/* fim da lógica usada no carrossel */
